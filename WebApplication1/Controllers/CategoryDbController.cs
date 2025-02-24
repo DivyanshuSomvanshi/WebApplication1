65a5c5e2b0d8132ca9f67144fd8722ee60b9f1d6
DivyanshuSomvanshi <divyanshusinghraw705@gmail.com>
2025-02-24 05:47:37 +0000

    test 5

---
+++ WebApplication1/Controllers/CategoryDbController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryDbController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public CategoryDbController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<CategoryDbController>
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return _context.Categories;
+        }
+
+        // GET api/<CategoryDbController>/5
+        [HttpGet("{id}")]
+        public Category Get(int id)
+        {
+            return _context.Categories.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/<CategoryDbController>
+        [HttpPost]
+        public void Post([FromBody]Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+        }
+
+        // PUT api/<CategoryDbController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Category category)
+        {
+            var categoryfromdb = _context.Categories.Find(id);
+            _context.Categories.Update(categoryfromdb);
+            _context.SaveChanges();
+        }
+
+        // DELETE api/<CategoryDbController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WebApplication1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
